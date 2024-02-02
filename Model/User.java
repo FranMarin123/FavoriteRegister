f711ff470b527fa07ccc0d1efb9f2a98a6ef0a0c
Juan <jsercas2101@g.educaand.es>
2024-02-02 12:18:31 +0100

    Integracion del usuario

---
+++ Model/User.java
@@ -0,0 +1,36 @@
+package Model;
+
+public class User extends Favorite {
+    private String username;
+    private String password;
+
+    public User(String name, int score, int year, String comment, String username, String password) {
+        super(name, score, year, comment);
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
