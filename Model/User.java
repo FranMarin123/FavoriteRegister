8f2054152918d4b808893c9273ced3fb5f466ff1
Juan <jsercas2101@g.educaand.es>
2024-02-02 13:02:26 +0100

    Modificar User Model

--- Model/User.java
+++ Model/User.java
@@ -1,11 +1,12 @@
 package Model;
 
-public class User extends Favorite {
+import java.util.Objects;
+
+public class User {
     private String username;
     private String password;
 
-    public User(String name, int score, int year, String comment, String username, String password) {
-        super(name, score, year, comment);
+    public User(String username, String password) {
         this.username = username;
         this.password = password;
     }
@@ -24,6 +25,19 @@ public class User extends Favorite {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(username, user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username);
     }
 
     @Override
